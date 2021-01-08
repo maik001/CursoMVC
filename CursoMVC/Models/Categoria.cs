8fcf5d394d8837278ca33d72b42f85eb08f40b9e
maik001 <maikemir2003@gmail..com>
2021-01-08 01:27:48 -0300

    Empurrando API, MVC e terminando teste

---
+++ CursoMVC/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id{ get; set; }
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+    }
+}
